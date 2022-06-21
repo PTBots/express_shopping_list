72681256a4e5db6184dabc03a0f814c952dc1ec6
PTBots <76126424+PTBots@users.noreply.github.com>
2022-06-21 12:07:28 -0600

    express-shopping-list

---
+++ item.js
@@ -0,0 +1,49 @@
+/**Items in your cart */
+const items = require('./fakeDb')
+
+class Item {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+
+        items.push(this);
+    }
+
+    static findItems() {
+        return items
+    }
+
+    /** Update items */
+    static update(name, data) {
+        let foundItem = Item.find(name);
+        if (foundItem === undefined) {
+            throw {message: 'Item not found', status: 404}
+        }
+        foundItem.name = data.name;
+        foundItem.price = data.price;
+
+        return foundItem;
+    }
+
+    /** Search items */
+
+    static search(name) {
+        const foundItem = items.search(i => i.name === name);
+        if (foundItem === undefined) {
+            throw {message: 'Item not found', status: 404}
+        }
+        return foundItem;
+    }
+
+    /** Delete item */
+
+    static delete(name) {
+        let foundItem = items.findItems(i => i.name === name);
+        if (foundItem === -1) {
+            throw {message: 'Item not found', status: 404}
+        }
+        items.splice(foundItem, 1);
+    }
+}
+
+module.exports = Item;
---
+++ routes/items.test.js
@@ -0,0 +1,94 @@
+process.env.NODE_ENV = 'test';
+// npm packages
+const request = require('supertest');
+// app imports
+const app = require('../app');
+
+let items = require('../fakeDb')
+
+let item = { name:'candy', price:1 }
+
+beforeEach(async() => {
+    items.push(item)
+});
+
+afterEach(async() => {
+    items = []
+});
+//end afterEach
+
+/** GET /items - returns `{items: [item, ...]}` */
+
+describe("GET /items", async function () {
+    test("Gets a list of items", async function() {
+        const response = await request(app).get(`/items`);
+        const {items} = response.body;
+        expect(response.statusCode).toBe(200);
+        expect(items).toHaveLength(1);
+    });
+});
+//end test
+
+/** GET /items[name] - return data about the item: `{item: item}` */
+
+describe("GET /items/:name", async function() {
+    test("Gets a single item", async function() {
+        const response = await request(app).get(`/items/${item.name}`);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.item).toEqual(item);
+    });
+
+    test('Responds with 404 if cannot find item', async function() {
+        const response = await request(app).get(`/items/0`);
+        expect(response.statusCode).toBe(404);
+    });
+});
+//end test
+
+/** POST /items --create item from data; return `{item: item}` */
+
+describe("POST /items", async function() {
+    test("Posts a new item", async function() {
+        const response = await request(app).post(`/items`).send({
+            name: "soda",
+            price: 1
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.item).toHaveProperty("name");
+        expect(response.body.item).toHaveProperty("price");
+        expect(response.body.item.name).toEqual('soda');
+        expect(response.body.item.price).toEqual(1);
+    });
+});
+//end test
+
+/** PATCH /items/[name] - update item; return `{item: item}` */
+
+describe("PATCH /items/:name", async function() {
+    test("Updates a single item", async function() {
+        const response = await request(app).patch(`/items/${item.name}`).send({
+            name: 'tea'
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.item).toEqual({
+            name: 'tea'
+        });
+    });
+
+    test('Responds with 404 if cannot find item', async function() {
+        const response = await request(app).patch(`/items/0`);
+        expect(response.statusCode).toBe(404);
+    });
+});
+//end test
+
+/** DELETE /items/[name] - delete item, return msg: `{message: 'item deleted'}` */
+
+describe('DELETE /items/:name', async function() {
+    test('Deletes a single item', async function() {
+        const response = await request(app).delete(`/items/${item.name}`);
+        expect(response.statusCode).toBe(200);
+        expect (response.body).toEqual({message: 'item deleted'});
+    });
+});
+//end test
