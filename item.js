f14e943a6cba89c0c2750e7e1cb3f9002c4a4e64
halim.tannous <halim.tannous@gmail.com>
2022-06-23 23:21:12 +0200

    post session

--- item.js
+++ item.js
@@ -1,49 +1,52 @@
 /**Items in your cart */
-const items = require('./fakeDb')
+const items = require("./fakeDb");
 
 class Item {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
 
-        items.push(this);
+    items.push(this);
+  }
+
+  static findItem(name) {
+    return items.find((i) => i.name == name);
+  }
+  static findItems(name) {
+    return items;
+  }
+
+  /** Update items */
+  static update(name, data) {
+    let foundItem = Item.search(name);
+    if (foundItem === undefined) {
+      throw { message: "Item not found", status: 404 };
     }
+    foundItem.name = data.name;
+    foundItem.price = data.price;
 
-    static findItems() {
-        return items
+    return foundItem;
+  }
+
+  /** Search items */
+
+  static search(name) {
+    const foundItem = items.find((i) => i.name === name);
+    if (foundItem === undefined) {
+      throw { message: "Item not found", status: 404 };
     }
+    return foundItem;
+  }
 
-    /** Update items */
-    static update(name, data) {
-        let foundItem = Item.find(name);
-        if (foundItem === undefined) {
-            throw {message: 'Item not found', status: 404}
-        }
-        foundItem.name = data.name;
-        foundItem.price = data.price;
+  /** Delete item */
 
-        return foundItem;
+  static delete(name) {
+    let foundItem = items.find((i) => i.name === name);
+    if (foundItem === -1) {
+      throw { message: "Item not found", status: 404 };
     }
-
-    /** Search items */
-
-    static search(name) {
-        const foundItem = items.search(i => i.name === name);
-        if (foundItem === undefined) {
-            throw {message: 'Item not found', status: 404}
-        }
-        return foundItem;
-    }
-
-    /** Delete item */
-
-    static delete(name) {
-        let foundItem = items.findItems(i => i.name === name);
-        if (foundItem === -1) {
-            throw {message: 'Item not found', status: 404}
-        }
-        items.splice(foundItem, 1);
-    }
+    items.splice(foundItem, 1);
+  }
 }
 
 module.exports = Item;
--- routes/items.js
+++ routes/items.js
@@ -1,60 +1,63 @@
-const Item = require('../item');
-const express = require('express');
-
+const Item = require("../item");
+const express = require("express");
+const expressError = require("../expressError");
 const router = express.Router();
 
 /**GET / => [item, ...] */
 
-router.get('', (req, res, next) => {
-    try {
-        return res.json({items: Item.findItems()});
-    } catch (err){
-        return next(err)
-    }
+router.get("", (req, res, next) => {
+  try {
+    return res.json({ items: Item.findItems() });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 /** POST / => new-item */
 
-router.post('', (req, res, next) => {
-    try {
-        let newItem = new Item(req.body.name, req.body.price);
-        return res.json({item: newItem});
-    } catch (err) {
-        return next(err)
-    }
+router.post("", (req, res, next) => {
+  try {
+    let newItem = new Item(req.body.name, req.body.price);
+    return res.json({ item: newItem });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 /** GET /[name] => item */
 
-router.get('/:name', (req, res, next) => {
-    try {
-        let foundItem = Item.findItems(req.params.name);
-        return res.json({item:foundItem});
-    } catch(err) {
-        return next(err)
+router.get("/:name", (req, res, next) => {
+  try {
+    let foundItem = Item.findItem(req.params.name);
+    if (!foundItem) {
+      throw new expressError("Item not found", 404);
     }
+    return res.json({ item: foundItem });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 /** PATCH /[name] => item */
 
-router.patch('/:name', (req, res, next) => {
-    try {
-        let foundItem = Item.update(req.params.name, req.body);
-        return res.json({ item: foundItem });
-    } catch(err) {
-        return next(err)
-    }
+router.patch("/:name", (req, res, next) => {
+  try {
+    let foundItem = Item.update(req.params.name, req.body);
+    return res.json({ item: foundItem });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 /** DELETE /[name] => item */
 
-router.delete('/:name', (req, res, next) => {
-    try {
-        Item.remove(req.params.name);
-        return res.json({message: 'Item deleted'});
-    } catch (err) {
-        return next(err)
-    }
+router.delete("/:name", (req, res, next) => {
+  try {
+    Item.delete(req.params.name);
+    return res.json({ message: "Item deleted" });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = router;
